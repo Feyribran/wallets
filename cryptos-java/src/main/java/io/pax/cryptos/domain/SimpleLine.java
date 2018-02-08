f9c92de2c5d1dca366979985e4323ad67b21b1f3
Feyribran <arnaud.vacherat@gmail.com>
2018-02-08 14:17:08 +0100

    remove comments on SimpleLine

--- cryptos-java/src/main/java/io/pax/cryptos/domain/SimpleLine.java
+++ cryptos-java/src/main/java/io/pax/cryptos/domain/SimpleLine.java
@@ -3,10 +3,10 @@ package io.pax.cryptos.domain;
 /**
  * Created by AELION on 08/02/2018.
  */
-/*public class SimpleLine implements Line {
+public class SimpleLine implements Line {
 
 
     double getSymbol();
 
     double getQuantity();
-}*/
+}
